f0418bd3b54db3fb920e5f294a11e1db79809c91
Daniel Parra <danialepaco@hotmail.com>
2018-04-30 23:11:46 -0400

    unit test

--- Golden GymTests/Golden_GymTests.swift
+++ Golden GymTests/Golden_GymTests.swift
@@ -13,24 +13,73 @@ class Golden_GymTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCalcularImc() {
+        
+        let controller = CalculatorController()
+        
+        let pesoExitoso = 60.0
+        let AlturaExitosa = 1.7
+        let imcExitoso = 20.7612457
+        
+        //Calculo Exitoso
+        XCTAssertEqual(controller.calcularIMC(peso: Float(pesoExitoso), altura: Float(AlturaExitosa)), Float(imcExitoso))
+        
+        
+        //Calculo Fallido
+
+        let imcFallido = 36.554
+        
+        XCTAssertNotEqual(controller.calcularIMC(peso: Float(pesoExitoso), altura: Float(AlturaExitosa)), Float(imcFallido))
+
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testClasificarImc() {
+        
+        let controller = CalculatorController()
+        
+        let bajo = 17
+        let normal = 19.5
+        let sobrepeso = 26.4
+        let obeso1 = 28.7
+        let obeso2 = 32.5
+        let obeso3 = 40.9
+        
+        let bajoMsj = "Peso Bajo. Necesario valorar signos de desnutricion."
+        let normalMsj = "Normal"
+        let sobrepesoMsj =  "Sobrepeso"
+        let obeso1Msj =  "Obesidad Grado I"
+        let obeso2Msj =  "Obesidad Grado II"
+        let obeso3Msj =  "Obesidad Grado III (Extrema o mórbida)"
+
+        //Clasificacion Exitosa
+        XCTAssertEqual(controller.clasificar(imc: Float(bajo)), bajoMsj)
+        XCTAssertEqual(controller.clasificar(imc: Float(normal)), normalMsj)
+        XCTAssertEqual(controller.clasificar(imc: Float(sobrepeso)), sobrepesoMsj)
+        XCTAssertEqual(controller.clasificar(imc: Float(obeso1)), obeso1Msj)
+        XCTAssertEqual(controller.clasificar(imc: Float(obeso2)), obeso2Msj)
+        XCTAssertEqual(controller.clasificar(imc: Float(obeso3)), obeso3Msj)
+        
+        //Clasificacion Fallida
+        let bajoMsjFallido =  "Obesidad Grado III (Extrema o mórbida)"
+        let normalMsjFallido =  "Obesidad Grado II"
+        let sobrepesoMsjFallido =  "Obesidad Grado I"
+        let obeso1MsjFallido =  "Sobrepeso"
+        let obeso2MsjFallido =  "Normal"
+        let obeso3MsjFallido = "Peso Bajo. Necesario valorar signos de desnutricion."
+        
+        XCTAssertNotEqual(controller.clasificar(imc: Float(bajo)), bajoMsjFallido)
+        XCTAssertNotEqual(controller.clasificar(imc: Float(normal)), normalMsjFallido)
+        XCTAssertNotEqual(controller.clasificar(imc: Float(sobrepeso)), sobrepesoMsjFallido)
+        XCTAssertNotEqual(controller.clasificar(imc: Float(obeso1)), obeso1MsjFallido)
+        XCTAssertNotEqual(controller.clasificar(imc: Float(obeso2)), obeso2MsjFallido)
+        XCTAssertNotEqual(controller.clasificar(imc: Float(obeso3)), obeso3MsjFallido)
+
+
     }
-    
 }
